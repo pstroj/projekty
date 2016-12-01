a820f769bcaf5153e1574e296be41f3355b03aba
pstrojwas <strojwas@icloud.com>
2016-12-01 19:24:00 +0100

    poprawa

--- spec/quicksort_spec.rb
+++ spec/quicksort_spec.rb
@@ -1,7 +1,7 @@
 require 'simplecov'
 SimpleCov.start
 
-require './quicksort'
+require 'quicksort'
 
 RSpec.describe 'quicksort.rb' do
   describe '#quicksort' do
@@ -14,19 +14,18 @@ RSpec.describe 'quicksort.rb' do
     end
  it 'prawidłowe sortowanie na intach' do
           expect(quicksort([122, 21, 332]))
-          eq([1, 122, 332])
+          eq([21, 122, 332])
+end	
+end
+ it 'brak strat' do
+ 	expect (quicksort([3,11,24,9]))
+  	contain_exactly(3,9,11,24)
+	end
+
+ it 'sort stringa  wedlug alfabetu' do
+        expect(quicksort(%w(ala ma kota))).to
+	 eq(%w(ala kota ma))
     end
-    
-    it 'pusta tablica' do
-          expect { quicksort([]).to be_a_kind_of(Boolean) }
-    end  
-    it 'method returns the correct boolean' do
-      expect(starts_with_vowel?('asdfgh'))
-        .to be(true), 'Incorrect results for input: \"aaaaaaaaaa\"'
-      expect(starts_with_vowel?('Veeeeeeee'))
-        .to be(false), 'Incorrect results for input: \"nooooooooo\"'
-    end
-  end
 
   describe '#binary_multiple_of_4?' do
     it 'should be defined' do
@@ -34,9 +33,9 @@ RSpec.describe 'quicksort.rb' do
     end
 
     it 'returns the correct boolean' do
-      expect(binary_multiple_of_4?('111111101'))
+      expect(quicksort('111111101'))
         .to be(false), 'Incorrect results for input: \"111111101\"'
-      expect(binary_multiple_of_4?('1010101010100'))
+      expect(quicksort('1010101010100'))
         .to be(true), 'Incorrect results for input: \"1010101010100\"'
     
    end
